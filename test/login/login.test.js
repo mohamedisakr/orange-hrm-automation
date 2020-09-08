1dc5b7b8bae59542050f438674b9eb1823372314
mohamedisakr <mohamedisakr@gmail.com>
2020-09-08 12:27:40 +0200

    checking when successful login page

--- test/login/login.test.js
+++ test/login/login.test.js
@@ -1,16 +1,20 @@
-const { Builder, By, Key, until } = require("selenium-webdriver");
+const { Builder, until } = require("selenium-webdriver"); //, By, Key, until
 const { login } = require("../../pages/login-form");
-// const assert = require("assert");
+const assert = require("chai").assert;
+const expect = require("chai").expect;
 
 describe("login-test", function () {
   this.timeout(30000);
   let driver;
   let vars;
   let url;
+  let actualUrl;
+  let expectedUrl;
 
   beforeEach(async function () {
     driver = await new Builder().forBrowser("chrome").build();
     url = "https://opensource-demo.orangehrmlive.com/index.php/auth/login";
+    actualUrl = "https://opensource-demo.orangehrmlive.com/index.php/dashboard";
     vars = {};
   });
 
@@ -22,6 +26,11 @@ describe("login-test", function () {
     await driver.get(url);
     await driver.manage().window().setRect(1366, 728);
     await login(driver);
+
+    await driver.wait(until.urlContains(actualUrl), 4000);
+    expectedUrl = await driver.getCurrentUrl((currentUrl) => currentUrl);
+    console.log(expectedUrl);
+    await expect(actualUrl).to.equal(expectedUrl);
   });
 });
 
